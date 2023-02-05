4653867058516335a10d3a99ac6b7b2c55ca0001
ammusubbu123 <118327045+ammusubbu123@users.noreply.github.com>
2023-02-05 19:33:09 +0530

    Create math.groovy

---
+++ vars/math.groovy
@@ -0,0 +1,14 @@
+def Sum(a,b)
+{
+   echo "The Sum of ${a} and ${b} is ${a+b}"
+}
+def Mul(a,b)
+{
+   echo "The prodeuct of ${a} and ${b} is ${a*b}"
+   
+}
+def Division(p,q)
+{
+   echo " The division of ${p} and ${q} is ${p/q}"
+   
+ }
